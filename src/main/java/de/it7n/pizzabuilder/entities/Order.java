cd1dc9aae7d358da556c96ba7776904ed333f599
ThorgeWohlers <thorgewohlers99@gmail.com>
2018-11-13 11:03:58 +0100

    test

--- src/main/java/de/it7n/pizzabuilder/entities/Order.java
+++ src/main/java/de/it7n/pizzabuilder/entities/Order.java
@@ -1,5 +1,6 @@
 package de.it7n.pizzabuilder.entities;
 
+import com.github.collinalpert.java2db.annotations.ForeignKeyEntity;
 import com.github.collinalpert.java2db.annotations.TableName;
 import com.github.collinalpert.java2db.entities.BaseEntity;
 
@@ -7,5 +8,27 @@ import com.github.collinalpert.java2db.entities.BaseEntity;
 public class Order extends BaseEntity {
     private double totalPrice;
     private int customerId;
+    @ForeignKeyEntity("customerId")
+    private Customer customer;
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
 
 }
