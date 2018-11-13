e40bf3eb9ad9fb826f7f80613309658de6c4d208
Collin Alpert <collinalpert@gmail.com>
2018-11-13 11:13:06 +0100

    Added services.

--- src/main/java/de/it7n/pizzabuilder/entities/Order.java
+++ src/main/java/de/it7n/pizzabuilder/entities/Order.java
@@ -6,29 +6,29 @@ import com.github.collinalpert.java2db.entities.BaseEntity;
 
 @TableName("order")
 public class Order extends BaseEntity {
-    private double totalPrice;
-    private int customerId;
-    @ForeignKeyEntity("customerId")
-    private Customer customer;
+	private double totalPrice;
+	private int customerId;
+	@ForeignKeyEntity("customerId")
+	private Customer customer;
 
-    public double getTotalPrice() {
-        return totalPrice;
-    }
+	public double getTotalPrice() {
+		return totalPrice;
+	}
 
-    public void setTotalPrice(double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
 
-    public int getCustomerId() {
-        return customerId;
-    }
+	public int getCustomerId() {
+		return customerId;
+	}
 
-    public void setCustomerId(int customerId) {
-        this.customerId = customerId;
-    }
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
 
-    public Customer getCustomer() {
-        return customer;
-    }
+	public Customer getCustomer() {
+		return customer;
+	}
 
 }
---
+++ src/main/java/de/it7n/pizzabuilder/services/CustomerService.java
@@ -0,0 +1,11 @@
+package de.it7n.pizzabuilder.services;
+
+import com.github.collinalpert.java2db.services.BaseService;
+import de.it7n.pizzabuilder.entities.Customer;
+
+/**
+ * @author Collin Alpert
+ */
+public class CustomerService extends BaseService<Customer> {
+
+}
