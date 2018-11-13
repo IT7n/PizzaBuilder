aff322e13a9cc3c21d2724d8afeedc2ed271bb47
Collin Alpert <collinalpert@gmail.com>
2018-11-13 10:56:56 +0100

    Added entities and database connection.

--- src/main/java/de/it7n/pizzabuilder/PizzaBuilderApplication.java
+++ src/main/java/de/it7n/pizzabuilder/PizzaBuilderApplication.java
@@ -1,12 +1,48 @@
 package de.it7n.pizzabuilder;
 
+import com.github.collinalpert.java2db.database.DBConnection;
+import com.github.collinalpert.java2db.utilities.IoC;
+import de.it7n.pizzabuilder.entities.Pizza;
+import de.it7n.pizzabuilder.services.PizzaService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PizzaBuilderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PizzaBuilderApplication.class, args);
+		var context = SpringApplication.run(PizzaBuilderApplication.class, args);
+		context.getBean(Instance.class).initialize();
+	}
+
+	@Bean
+	Instance getInstance() {
+		return new Instance();
+	}
+
+	private static class Instance {
+
+		@Value("${database.host}")
+		private String host;
+
+		@Value("${database.name}")
+		private String database;
+
+		@Value("${database.user}")
+		private String userName;
+
+		@Value("${database.password}")
+		private String password;
+
+		public void initialize() {
+			DBConnection.HOST = host;
+			DBConnection.DATABASE = database;
+			DBConnection.USERNAME = userName;
+			DBConnection.PASSWORD = password;
+
+			IoC.registerService(Pizza.class, new PizzaService());
+		}
 	}
 }
---
+++ src/main/java/de/it7n/pizzabuilder/entities/Customer.java
@@ -0,0 +1,57 @@
+package de.it7n.pizzabuilder.entities;
+
+import com.github.collinalpert.java2db.annotations.TableName;
+import com.github.collinalpert.java2db.entities.BaseEntity;
+
+/**
+ * @author Collin Alpert
+ */
+@TableName("customer")
+public class Customer extends BaseEntity {
+
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String password;
+	private String email;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
