3329c1f9c27a26a6dbe1dd3d6b76b12da7e36dbf
Collin Alpert <collinalpert@gmail.com>
2018-11-19 12:40:48 +0100

    Added missing methods

--- src/main/java/de/it7n/pizzabuilder/entities/Customer.java
+++ src/main/java/de/it7n/pizzabuilder/entities/Customer.java
@@ -27,8 +27,15 @@ public class Customer extends BaseEntity {
 		return address;
 	}
 
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public String getEmail() {
 		return email;
 	}
-
 }
